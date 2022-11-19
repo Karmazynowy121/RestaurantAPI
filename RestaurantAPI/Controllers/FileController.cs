d5bb49375d05872a2cc13580dbbbd3098e9aebfc
Karmazynowy121 <atak121@wp.pl>
2022-11-19 14:21:52 +0100

    innit

--- RestaurantAPI/Controllers/FileController.cs
+++ RestaurantAPI/Controllers/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using System.IO;
@@ -6,9 +7,10 @@ using System.IO;
 namespace RestaurantAPI.Controllers
 {
     [Route("file")]
-    [Authorize]
     public class FileController : ControllerBase
     {
+        [HttpGet]
+        [ResponseCache(Duration = 1200, VaryByQueryKeys = new[] {"fileName"})]
         public ActionResult GetFile([FromQuery] string fileName)
         {
             var roothPath = Directory.GetCurrentDirectory();
@@ -27,7 +29,23 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, fileName);
+        }
 
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
